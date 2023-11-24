3204af3640e353d0a9484d2b59caa68ee5cc7517
Kim Lasse Massesson <kima4@elev.tec.dk>
2023-11-24 11:19:33 +0100

    Add project files.

---
+++ Data.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Data
+    {
+        public static void BannerText(int x, int y)
+        {
+            Pos(x, y, @"    Lommeregner", ConsoleColor.White);
+        }
+        public static void Banner(int x, int y)
+        {
+            Pos(x, y + 0, @"*************************", ConsoleColor.Red);
+            Pos(x, y + 1, @"*", ConsoleColor.Red);
+            Pos(x + 24, y + 1, @"*", ConsoleColor.Red);
+            Pos(x, y + 2, @"*************************", ConsoleColor.Red);
+        }
+
+
+
+            public static void NumLockKeys(int x, int y)
+        {
+            Pos(x, y + 0, @"┌───────────────────────┐", ConsoleColor.Yellow);
+            Pos(x, y + 1, @"│                       │", ConsoleColor.Yellow);
+            Pos(x, y + 2, @"│                       │", ConsoleColor.Yellow);
+            Pos(x, y + 3, @"│                       │", ConsoleColor.Yellow);
+            Pos(x, y + 4, @"│                       │", ConsoleColor.Yellow);
+            Pos(x, y + 5, @"├─────┬─────┬─────┬─────┤", ConsoleColor.Yellow);
+            Pos(x, y + 6, @"│  7  │  8  │  9  │  *  │", ConsoleColor.Yellow);
+            Pos(x, y + 7, @"├─────┼─────┼─────┼─────┤", ConsoleColor.Yellow);
+            Pos(x, y + 8, @"│  4  │  5  │  6  │  /  │", ConsoleColor.Yellow);
+            Pos(x, y + 9, @"├─────┼─────┼─────┼─────┤", ConsoleColor.Yellow);
+            Pos(x, y + 10, @"│  1  │  2  │  3  │  -  │", ConsoleColor.Yellow);
+            Pos(x, y + 11, @"├─────┼─────┼─────┼─────┤", ConsoleColor.Yellow);
+            Pos(x, y + 12, @"│  0  │     │  =  │  +  │", ConsoleColor.Yellow);
+            Pos(x, y + 13, @"└─────┴─────┴─────┴─────┘", ConsoleColor.Yellow);
+
+        }
+
+        public static void Pos(int x, int y, object tekst, ConsoleColor color = ConsoleColor.White) //positioning text
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(x, y);
+            Console.Write(tekst);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Calculator
+{
+    internal class Program
+    {
+        public static double num1;
+        public static double num2;
+
+
+        static void Main(string[] args)
+        {
+            //setting up the calculator
+            Data.Banner(10, 2);
+            Data.BannerText(13, 3);
+            Data.NumLockKeys(10, 5);
+            Console.CursorVisible = true;
+            //starting the program
+            GetInput();
+        }
+        /// <summary>
+        /// getting input from user and putting it into the claculator
+        /// </summary>
+        static void GetInput()
+        {
+            Data.Pos(11, 21, "Tast ENTER efter hvert tal");
+            Console.SetCursorPosition(13, 6);
+            string num1a = Console.ReadLine();
+            Console.SetCursorPosition(13, 7);
+            num1 = double.Parse(num1a);
+
+            char input = Console.ReadKey().KeyChar;
+
+            Console.SetCursorPosition(13, 8);
+            string num2a = Console.ReadLine();
+            num2 = double.Parse(num2a);
+
+                GetChoice(input);
+            Console.SetCursorPosition(0, 26);
+
+        }
+
+        /// <summary>
+        /// Switch checking operator
+        /// </summary>
+        /// <param name="choice"></param>
+        static void GetChoice(char choice)
+        {
+            switch (choice)
+            {
+                case '+':
+                    PerformAddition();
+                    break;
+                case '-':
+                    PerformSubtraction();
+                    break;
+                case '*':
+                    PerformMultiplication();
+                    break;
+                case '/':
+                    PerformDivision();
+                    break;
+                default:
+                    Data.Pos(11, 21, "Du skal taste et gyldigt tegn");
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Performing addition
+        /// </summary>
+        static void PerformAddition()
+        {
+
+            double result = num1 + num2;
+        static double GetNumberInput()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (double.TryParse(input, out double number))
+                {
+                    return number;
+                }
+                else
+                {
+                    Data.Pos(11, 21, "Du skal taste et gyldigt tal");
+                }
+            }
+        }
+            Data.Pos(13, 9, $"= {result}");
+        }
+
+        /// <summary>
+        /// Performing subtraction
+        /// </summary>
+        static void PerformSubtraction()
+        {
+            double result = num1 - num2;
+            Data.Pos(13, 9, $"= {result}");
+        }
+
+        static void PerformMultiplication()
+        {
+            double result = num1 * num2;
+            Data.Pos(13, 9, $"= {result}");
+        }
+
+        /// <summary>
+        /// Performing division
+        /// </summary>
+        static void PerformDivision()
+        {
+            if (num2 != 0)
+            {
+                double result = num1 / num2;
+                Data.Pos(13, 9, $" = {result}");
+            }
+            else
+            {
+                Data.Pos(11, 21, "Error: Man kan ikke dividere med 0.");
+            }
+        }
+    }
+}
